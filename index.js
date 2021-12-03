e4ae8342dcb68d2fe2b9df5b546cd5bb6c889f22
danmera1980 <danmera1980@gmail.com>
2021-12-03 16:10:53 -0500

    Mostrar datos de los equipos

--- index.js
+++ index.js
@@ -18,7 +18,29 @@ http.createServer((req, res) => {
             res.writeHead(404, {'Content-Type' : 'text/plain'})
             res.end('No hay informacion \n' + err)
         })
-    } else {
+    } else if(req.url === '/api/equipos'){
+        let allInfo = new Promise((resolve, reject) => {
+            fs.readFile('./data.json', (err, data) => {
+                if(err) return reject(err)
+                resolve({data, contenType: 'application/json', status: 200})
+            })
+        })
+
+        allInfo.then(data => {
+            res.writeHead(data.status, {'Content-Type' : data.contenType})
+            let resultados = JSON.parse(data.data.toString('utf-8'))
+            let equipos = []
+            resultados.asociaciones.forEach(key => {
+                equipos.push(key.equipo)
+            })
+            console.log(equipos)
+            // console.log(Object.keys(resultados.asociaciones))
+            res.end(JSON.stringify(equipos))
+        }).catch(err => {
+            res.writeHead(404, {'Content-Type' : 'text/plain'})
+            res.end('No hay informacion \n' + err)
+        })
+    }else {
         res.writeHead(404, {'Content-Type' : 'text/plain'})
         res.end('No hay esa ruta \n')
     }
