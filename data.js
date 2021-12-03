87ec25db83c0b8b4250c28dfb401b34ca317e5a1
danmera1980 <danmera1980@gmail.com>
2021-12-03 09:35:29 -0500

    Project Created and JSON template organized

---
+++ data.js
@@ -0,0 +1,96 @@
+export default [
+    {
+        asociaciones: [
+            {
+                id: 1,
+                pais: 'Argentina',
+                nombre: 'Asociación del Fútbol Argentino - AFA',
+                fundacion: '21/02/1893',
+                sede: 'Viamonte 1366, (1053) Buenos Aires',
+                presidente: 'Claudio Tapia',
+                web: 'http://www.afa.org.ar/#',
+                imgLogo: '',
+                detalle: 'La República Argentina es cuna del primer partido jugado en América del Sur, propiciado por los ingleses que construían las vías del ferrocarril. Fue utilizada una vejiga vacuna para el efecto. En el año 1882, Alejandro Watson Hutton, llega a Buenos Aires, con una pelota e inflador. .. De ahí en más creció el movimiento más extraordinario de la Argentina. Tiene dos títulos mundiales, 14 Copa América, títulos olímpicos y a nivel de clubes es un potrero... Alfredo Di Stéfano, Diego Maradona y Lionel Messi, símbolos universales de fútbol y argentinidad...',
+                equipo: [
+                    {
+                        id: 1,
+                        pais: 'Argentina',
+                        nombre: 'Argentina',
+                        dt: 'Lionel Scaloni',
+                        jugadores: [
+                            {
+                                id: 1,
+                                nombre: 'Franco',
+                                apellido: 'Armani',
+                                DOB: '16/10/1986',
+                                numero: '1',
+                                posicion: 'Arquero',
+                                nacionalidad: 'Argentino',
+                                equipoActual: 'River Plate',
+                                imagen: 'https://as01.epimg.net/img/comunes/fotos/fichas/deportistas/f/fra/100/28879.jpg'
+                            },
+                            {
+                                id: 2,
+                                nombre: '',
+                                apellido: '',
+                                DOB: '',
+                                numero: '',
+                                posicion: '',
+                                nacionalidad: '',
+                                equipoActual: '',
+                                imagen: ''
+                            },
+                        ]
+                    }
+                ]
+            },
+            {
+                id: 2,
+                pais: '',
+                nombre: '',
+                fundacion: '',
+                sede: '',
+                presidente: '',
+                web: '',
+                imgLogo: '',
+                detalle: '',
+                equipo: [
+                    {
+                        id: 1,
+                        pais: '',
+                        nombre: '',
+                        dt: '',
+                        jugadores: [
+                            {
+                                id: 1,
+                                nombre: '',
+                                apellido: '',
+                                DOB: '',
+                                numero: '',
+                                posicion: '',
+                                nacionalidad: '',
+                                equipoActual: '',
+                                imagen: ''
+                            },
+                            {
+                                id: 2,
+                                nombre: '',
+                                apellido: '',
+                                DOB: '',
+                                numero: '',
+                                posicion: '',
+                                nacionalidad: '',
+                                equipoActual: '',
+                                imagen: ''
+                            },
+                        ]
+                    }
+                ]
+            },
+        ]
+    },
+]
+
+// Información de las Asociaciones se puede obtener del siguiente link: https://www.conmebol.com/es/laconmebol/asociaciones
+// Información de los equipos se puede obtener del siguiente link: https://www.conmebol.com/es/eliminatorias-sudamericanas-catar-2022
+// Información de los jugadores se puede obtener del siquiente link: https://resultados.as.com/resultados/ficha/listado/
---
+++ index.js
@@ -0,0 +1,7 @@
+var fs = require('fs');
+var http = require('http')
+var data = require('./data')
+
+http.createServer((req, res) => {
+
+}).listen(8888, '127.0.0.1')
