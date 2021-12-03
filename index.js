10947c91854de882c8bd55e99b2e5e2b52f70caf
danmera1980 <danmera1980@gmail.com>
2021-12-03 11:02:22 -0500

    Updated json file. Created api for all info.

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 var fs = require('fs');
-var http = require('http')
-var data = require('./data')
+var http = require('http');
 
 http.createServer((req, res) => {
+    if(req.url === '/api') {
 
+        let allInfo = new Promise((resolve, reject) => {
+            fs.readFile('./data.json', (err, data) => {
+                if(err) return reject(err)
+                resolve({data, contenType: 'application/json', status: 200})
+            })
+        })
+
+        allInfo.then(data => {
+            res.writeHead(data.status, {'Content-Type' : data.contenType})
+            res.end(data.data)
+        }).catch(err => {
+            res.writeHead(404, {'Content-Type' : 'text/plain'})
+            res.end('No hay informacion \n' + err)
+        })
+    } else {
+        res.writeHead(404, {'Content-Type' : 'text/plain'})
+        res.end('No hay esa ruta \n')
+    }
 }).listen(8888, '127.0.0.1')
